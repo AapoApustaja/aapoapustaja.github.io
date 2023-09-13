d7c0d6ce71718b3d8e98d5c21e9f084d1a13a941
AapoApustaja <aapolarsen29@gmail.com>
2023-09-13 15:16:32 +0300

    websitege

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateTime() {
+  const clockElement = document.getElementById("current-time");
+  const now = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+    timeZone: "Europe/Helsinki",
+  };
+  const timeString = now.toLocaleTimeString("en-GB", options);
+  clockElement.textContent = timeString;
+
+  setTimeout(updateTime, 1000); // Update the clock every second
+}
+
+updateTime();
