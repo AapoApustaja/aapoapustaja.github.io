92482e1a80ab3f2b27ca0af2d6ec3cecb7394e03
AapoApustaja <aapolarsen29@gmail.com>
2023-10-01 23:18:44 +0300

    asd

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# aapoapustaja.github.io
+# Websitege
 
 ![58b85585972e0f04b20e1f31ac5e6c75](https://github.com/AapoApustaja/AapoBot/assets/138826787/e6d36fd3-d270-459c-8aae-094ce2995061)
--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-function updateTime() {
-  const clockElement = document.getElementById("current-time");
-  const now = new Date();
-  const options = {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: false,
-    timeZone: "Europe/Helsinki",
-  };
-  const timeString = now.toLocaleTimeString("en-GB", options);
-  clockElement.textContent = timeString;
-  setTimeout(updateTime, 1000);
-}
-
-updateTime();
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
